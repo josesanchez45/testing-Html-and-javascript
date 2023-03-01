56db644334d5eccdc436f6a1ca6489e0e054a9ea
jose sanchez <jose.sanchez.artist@gmail.com>
2023-03-01 15:04:58 -0800

    test HTML/ JavaScript

---
+++ app.js
@@ -0,0 +1,13 @@
+let m = 1;
+do{
+    let answer = ""
+    if(m % 3 == 0){
+        answer += 'Fizz'
+    }
+    if(m % 5 == 0){
+        answer += 'Buzz'
+    console.log(`${m} ${answer}`)
+
+    m++;
+    }
+}while(m <= 100)
